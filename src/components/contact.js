f81d86312ab70c7247173ba48550ca6a30b71370
fstuardom <fstuardom@gmail.com>
2021-02-22 10:39:00 -0300

    cards update

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,58 +1,82 @@
-import React, { Component } from 'react';
-import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 
 class Contact extends Component {
-	render() {
-		return (
-			<div className="contact-body">
-				<Grid className="contact-grid">
-					<Cell col={6}>
-						<h2>Felipe Stuardo</h2>
-						<img src="https://cdn3.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png" alt="avatar" />
-						<p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}> Let's Build something.</p>
-					</Cell>
-					<Cell col={6}>
-						<h2>Get in touch with me</h2>
-						<hr />
-						<div className="contact-list">
-							<List className="list">
-								<ListItem>
-									<ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
-										<i
-											style={{ paddingLeft: '1em' }}
-											className="fa fa-phone-square"
-											aria-hidden="true"
-										/>
-										+56961687465
-									</ListItemContent>
-								</ListItem>
-								<ListItem>
-									<ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
-										<i
-											style={{ paddingLeft: '1em' }}
-											className="fa fa-envelope-square"
-											aria-hidden="true"
-										/>
-										fstuardom@gmail.com
-									</ListItemContent>
-								</ListItem>
-								<ListItem>
-									<ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
-										<i
-											style={{ paddingLeft: '1em' }}
-											className="fa fa-linkedin-square"
-											aria-hidden="true"
-										/>
-										LinkedinLink
-									</ListItemContent>
-								</ListItem>
-							</List>
-						</div>
-					</Cell>
-				</Grid>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Felipe Stuardo</h2>
+            <img
+              src="https://cdn3.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+            />
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              {" "}
+              Let's Build something.
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Get in touch with me</h2>
+            <hr />
+            <div className="contact-list">
+              <List className="list">
+                <ListItem>
+                  <ListItemContent
+                    style={{
+                      fontSize: "25px",
+                      fontFamily: "Anton",
+                      display: "flex",
+                    }}
+                  >
+                    <i
+                      style={{ paddingLeft: "1em" }}
+                      className="fa fa-phone-square"
+                      aria-hidden="true"
+                    />
+                    +56961687465
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent
+                    style={{
+                      fontSize: "25px",
+                      fontFamily: "Anton",
+                      display: "flex",
+                    }}
+                  >
+                    <i
+                      style={{ paddingLeft: "1em" }}
+                      className="fa fa-envelope-square"
+                      aria-hidden="true"
+                    />
+                    fstuardom@gmail.com
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent
+                    style={{
+                      fontSize: "25px",
+                      fontFamily: "Anton",
+                      display: "flex",
+                    }}
+                  >
+                    <i
+                      style={{ paddingLeft: "1em" }}
+                      className="fa fa-linkedin-square"
+                      aria-hidden="true"
+                    />
+                    LinkedinLink
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Contact;
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,126 +1,135 @@
-import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton, CardText } from 'react-mdl';
-import projectone from '../images/fullimg.png';
+import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton,
+  CardText,
+} from "react-mdl";
+import projectone from "../images/fullimg.png";
 
 class Projects extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { activeTab: 0 };
-	}
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
 
-	render() {
-		return (
-			<div className="category-tabs">
-				<Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-					<Tab>Projects</Tab>
-				</Tabs>
-				<div className="projects-grid">
-					<Card className="card-projects">
-						<CardTitle
-							style={{
-								color      : 'grey',
-								height     : '280px',
-								background : `url(${projectone}) center / cover`
-							}}
-						>
-							{'The MERN Stack'}
-						</CardTitle>
-						<CardText>
-							Ecommerce webpage with authentification and paypal payment using MongoDB, ExpressJS, React
-							and NodeJS
-						</CardText>
-						<CardActions border>
-							<a href="http://bestshopmern.herokuapp.com/" target="_blank" rel="noreferrer">
-								<Button colored>Live Version</Button>
-							</a>
-							<Button colored>GitHub</Button>
-						</CardActions>
-						<CardMenu style={{ color: '#00000' }}>
-							<IconButton name="share" />
-						</CardMenu>
-					</Card>
-					<Card className="card-projects">
-						<CardTitle
-							style={{
-								color      : '#000000',
-								height     : '280px',
-								background : 'url(https://cdn.auth0.com/blog/react-js/react.png) center / cover'
-							}}
-						>
-							{'Javascript Shopping App'}
-						</CardTitle>
-						<CardText>
-							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-						</CardText>
-						<CardActions border>
-							<Button colored>Live Demo</Button>
-							<Button colored>Live Demo</Button>
-						</CardActions>
-						<CardMenu style={{ color: '#00000' }}>
-							<IconButton name="share" />
-						</CardMenu>
-					</Card>
-					<Card className="card-projects">
-						<CardTitle
-							style={{
-								color      : '#000000',
-								height     : '280px',
-								background : 'url(https://cdn.auth0.com/blog/react-js/react.png) center / cover'
-							}}
-						>
-							{'Javascript Shopping App'}
-						</CardTitle>
-						<CardText>
-							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-							been the industry's standard dummy
-						</CardText>
-						<CardActions border>
-							<Button colored>Live Demo</Button>
-							<Button colored>Live Demo</Button>
-						</CardActions>
-						<CardMenu style={{ color: '#00000' }}>
-							<IconButton name="share" />
-						</CardMenu>
-					</Card>
-				</div>
-
-				<section>
-					<Grid>
-						<Cell col={12}>
-							<div className="content" />
-						</Cell>
-					</Grid>
-				</section>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={(tabId) => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>Projects</Tab>
+        </Tabs>
+        <div className="projects-grid">
+          <Grid className="grid-project">
+            <Cell col={4}>
+              <Card
+                className="card-projects"
+                shadow={0}
+                style={{ width: "320px", height: "320px", margin: "auto" }}
+              >
+                <CardTitle
+                  style={{
+                    color: "grey",
+                    height: "280px",
+                    background: `url(${projectone}) center / cover`,
+                  }}
+                >
+                  {"The MERN Stack"}
+                </CardTitle>
+                <CardText>
+                  Ecommerce webpage with authentification and paypal payment
+                  using MongoDB, ExpressJS, React and NodeJS
+                </CardText>
+                <CardActions border>
+                  <a
+                    href="http://bestshopmern.herokuapp.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Button colored>Live Version</Button>
+                  </a>
+                  <Button colored>GitHub</Button>
+                </CardActions>
+                <CardMenu style={{ color: "#00000" }}>
+                  <IconButton name="share" />
+                </CardMenu>
+              </Card>
+            </Cell>
+            <Cell col={4}>
+              <Card
+                className="card-projects"
+                shadow={0}
+                style={{ width: "320px", height: "320px", margin: "auto" }}
+              >
+                <CardTitle
+                  style={{
+                    color: "#000000",
+                    height: "280px",
+                    background:
+                      "url(https://cdn.auth0.com/blog/react-js/react.png) center / cover",
+                  }}
+                >
+                  {"Javascript Shopping App"}
+                </CardTitle>
+                <CardText>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has
+                </CardText>
+                <CardActions border>
+                  <Button colored>Live Demo</Button>
+                  <Button colored>Live Demo</Button>
+                </CardActions>
+                <CardMenu style={{ color: "#00000" }}>
+                  <IconButton name="share" />
+                </CardMenu>
+              </Card>
+            </Cell>
+            <Cell col={4}>
+              <Card
+                className="card-projects"
+                shadow={0}
+                style={{ width: "320px", height: "320px", margin: "auto" }}
+              >
+                <CardTitle
+                  style={{
+                    color: "#000000",
+                    height: "280px",
+                    background:
+                      "url(https://cdn.auth0.com/blog/react-js/react.png) center / cover",
+                  }}
+                >
+                  {"Javascript Shopping App"}
+                </CardTitle>
+                <CardText>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy
+                </CardText>
+                <CardActions border>
+                  <Button colored>Live Demo</Button>
+                  <Button colored>Live Demo</Button>
+                </CardActions>
+                <CardMenu style={{ color: "#00000" }}>
+                  <IconButton name="share" />
+                </CardMenu>
+              </Card>
+            </Cell>
+          </Grid>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Projects;
-
-/* <Card className="card-projects">
-						<CardTitle
-							style={{
-								color      : 'grey',
-								height     : '200px',
-								background : `url(${projectone}) center / cover`
-							}}
-						>
-							{'The MERN Stack'}
-						</CardTitle>
-						<CardText>
-							Ecommerce webpage with authentification and paypal payment using MongoDB, ExpressJS, React
-							and NodeJS
-						</CardText>
-						<CardActions border>
-							<a href="http://bestshopmern.herokuapp.com/" target="_blank">
-								<Button colored>Live Demo</Button>
-							</a>
-							<Button colored>GitHub</Button>
-						</CardActions>
-						<CardMenu style={{ color: '#00000' }}>
-							<IconButton name="share" />
-						</CardMenu>
-					</Card>
-
-					*/
